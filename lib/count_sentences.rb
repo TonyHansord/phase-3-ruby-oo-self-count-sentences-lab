e0fc760cbfb6dc3e840ee0515b01676ba61c7f35
Anthony Hansord <tonyhansorddev@gmail.com>
2023-03-27 20:27:16 +1100

    completed lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self[-1] == "."
   end
 
   def question?
-
+    return self[-1] =="?"
   end
 
   def exclamation?
-
+    return self[-1] =="!"
   end
 
   def count_sentences
-
+    
+    self.split(/[.?!]/).reject {|string| string.empty?}.count 
+  
+    
   end
 end
+
+this_string = "This is a string. With two Sentences. Is this Ok? Yay!"
+
+this_string.count_sentences
